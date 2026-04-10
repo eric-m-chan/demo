3a420596c7df93814ca2a665c424f1a640162ade
eric-m-chan <echan044@ucr.edu>
2026-04-10 13:25:08 -0700

    alice change

--- main.cpp
+++ main.cpp
@@ -4,6 +4,7 @@ int main() {
 	std::cout << "hello world" << std::endl;
 	std::cout << "how are you doing?" << std::endl;
 	std::cout << "im doing well, and you?" << std::endl;
+	std::cout << "my name is alice" << std::endl;
 
 	return 0;
 }
