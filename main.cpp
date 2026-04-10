b53e19b7d0a013d40c9d783f49ff024b10e360e4
eric-m-chan <echan044@ucr.edu>
2026-04-10 13:22:30 -0700

    eric change

--- main.cpp
+++ main.cpp
@@ -4,6 +4,6 @@ int main() {
 	std::cout << "hello world" << std::endl;
 	std::cout << "how are you doing?" << std::endl;
 	std::cout << "im doing well, and you?" << std::endl;
-
+	std::cout << "my name is eric" << std::endl;
 	return 0;
 }
